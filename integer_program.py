cee36b019cfe0352a879577b7e2b67aa9a1f8db3
Grady Johnson <grady.johnson@target.com>
2015-10-20 14:27:14 -0500

    Added 'S' grade requirement

--- integer_program.py
+++ integer_program.py
@@ -4,19 +4,29 @@ print "### Is your integer increasing, decreasing, or neither? ###"
 print "###########################################################"
 print
 
-increasing = "--> Your integer is increasing.\n"
-decreasing = "--> Your integer is decreasing.\n"
-neither = "--> Your integer is neither increasing nor decreasing.\n"
-error = "--> You did not enter a valid integer. Please try again.\n"
+increasing = "--> Your integer is increasing."
+decreasing = "--> Your integer is decreasing."
+neither = "--> Your integer is neither increasing nor decreasing."
+error = "--> You did not enter a valid integer. Please try again."
+
+history_user_input = []
+history_classification = []
 
 while True:
-    user_input = raw_input("Please enter an integer: ")
+    user_input = raw_input("\nPlease enter an integer: ")
     if user_input.lower() == "end":
         print "\n=== SESSION TERMINATED ===\n"
         break
+    elif user_input.lower() == "history":
+        if history_user_input == [] or history_classification == []:
+            print "--> There is no history."
+        else:
+            for i, j in zip(history_user_input, history_classification):
+                print i, j
     else:
         try:
             user_input = abs(int(user_input)) #Attempts to convert user_input into a positive int
+            history_user_input.append(user_input)
         except ValueError: #Displays an error if user_input can't be converted into a positive int
             print error
             continue
@@ -24,7 +34,7 @@ while True:
     flag = 0
     old_flag = 0
 
-    while flag == old_flag:
+    while flag == old_flag and user_input != "history":
 
         first_num = user_input % 10
         user_input /= 10
@@ -47,8 +57,14 @@ while True:
 
     #Prints appropriate statement given flag
     if flag == 1 and old_flag == 1:
+        history_classification.append(increasing)
         print increasing
     elif flag == 2 and old_flag == 2:
+        history_classification.append(decreasing)
         print decreasing
     else:
-        print neither
+        if user_input == "history":
+            continue
+        else:
+            history_classification.append(neither)
+            print neither
